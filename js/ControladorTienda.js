6515b0c48cc4585bf049bb412c31ecf4c5ddefef
203 <203@academicos.cesde.edu.co>
2022-02-16 10:05:29 -0500

    crea controlador

---
+++ js/ControladorTienda.js
@@ -0,0 +1,35 @@
+// entradas del problema (comentario de linea)
+
+/*
+ comentario de bloque
+*/
+let nombreProducto="camara sony"
+let precioProducto = 3500000
+let pesoProducto = 300
+let tieneDescuento = true
+let descripcionProducto = "sony alpha 7"
+let teleofonoVendedor = "31367097964"
+
+const NOMBRE_CIELNTE = "pepito pérez"
+
+
+//arreglos unidimencionales (vectores)
+// sirven para almacenar varios elementos en 1 sola variable
+
+let productos= ["camara", "tripode", "flash", "memorias"]
+let productos2 = Array("camara", "tripode", "flash", "memorias")
+
+
+
+
+
+//salidas
+
+console.log ("buenas")
+console.log(tieneDescuento)
+console.log ("su nombre es: " + NOMBRE_CIELNTE + "y su producto es " + descripcionProducto)
+// `` alt 96 o alt gr }
+console.log(`su nombre es: ${NOMBRE_CIELNTE} y su producto es: ${nombreProducto}`)
+console.log("*******************")
+console.log (productos)
+console.log(productos2[0])
