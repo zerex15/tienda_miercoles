5f0934852e941bdc12395f2b1aae0c5afb4cba36
zerex15 <zerex1502@gmail.com>
2022-02-23 09:36:22 -0500

    creacion de productos

--- js/ControladorTienda.js
+++ js/ControladorTienda.js
@@ -1,35 +1,62 @@
-// entradas del problema (comentario de linea)
+// // entradas del problema (comentario de linea)
 
-/*
- comentario de bloque
+// /*
+//  comentario de bloque
+// */
+// let nombreProducto="camara sony"
+// let precioProducto = 3500000
+// let pesoProducto = 300
+// let tieneDescuento = true
+// let descripcionProducto = "sony alpha 7"
+// let teleofonoVendedor = "31367097964"
+
+// const NOMBRE_CIELNTE = "pepito pérez"
+
+
+// //arreglos unidimencionales (vectores)
+// // sirven para almacenar varios elementos en 1 sola variable
+
+// let productos= ["camara", "tripode", "flash", "memorias"]
+// let productos2 = Array("camara", "tripode", "flash", "memorias")
+
+
+
+
+
+// //salidas
+
+// console.log ("buenas")
+// console.log(tieneDescuento)
+// console.log ("su nombre es: " + NOMBRE_CIELNTE + "y su producto es " + descripcionProducto)
+// // `` alt 96 o alt gr }
+// console.log(`su nombre es: ${NOMBRE_CIELNTE} y su producto es: ${nombreProducto}`)
+// console.log("*******")
+// console.log (productos)
+// console.log(productos2[0])
+
+ /* //controlando etiquetas
+let etiquetaTitulo = document.getElementById("titulo")
+console.log(etiquetaTitulo)
+
+
+//controlando el menú
+let etiquetaMenu = document.getElementById("navbar")
+etiquetaMenu.textContent="Zerex_photo"
+
+//controlando una imagen
+let etiquetaFoto = document.getElementById("fotoFondo")
+etiquetaFoto.src="img/descarga.jpg"
+
+//cambiar el estilo de la etiqueta
+etiquetaTitulo.classList.add("text-center")
 */
-let nombreProducto="camara sony"
-let precioProducto = 3500000
-let pesoProducto = 300
-let tieneDescuento = true
-let descripcionProducto = "sony alpha 7"
-let teleofonoVendedor = "31367097964"
 
-const NOMBRE_CIELNTE = "pepito pérez"
+//TIENDA
 
+let nombreProducto = "lente sigma"
+let precioProducto = 4000000
+let descripcionProducto = "lente sigma 35mm"
 
-//arreglos unidimencionales (vectores)
-// sirven para almacenar varios elementos en 1 sola variable
-
-let productos= ["camara", "tripode", "flash", "memorias"]
-let productos2 = Array("camara", "tripode", "flash", "memorias")
-
-
-
-
-
-//salidas
-
-console.log ("buenas")
-console.log(tieneDescuento)
-console.log ("su nombre es: " + NOMBRE_CIELNTE + "y su producto es " + descripcionProducto)
-// `` alt 96 o alt gr }
-console.log(`su nombre es: ${NOMBRE_CIELNTE} y su producto es: ${nombreProducto}`)
-console.log("*******************")
-console.log (productos)
-console.log(productos2[0])
+let nombreProducto2 = "camara leica"
+let precioProducto2 = 15000000
+let descripcionProducto2 = "camara tipo vintage leica"
